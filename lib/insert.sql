6784f4d82bdc2aa8d425de2a049629b53a3a2944
Abdulaziz Abdi <amigf76@gmail.com>
2024-06-09 12:49:44 +0300

    bears SQL lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ( 'Mr. Chocolate', 2, 'M', 'black', 'good', 0);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ( 'Rowdy', 3, 'M', 'black', 'good', 0);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ( 'Tabitha', 5, 'F', 'black', 'good', 1);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ( 'Sergeant Brown', 7, 'M', 'black', 'good', 1);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ( 'Melissa', 12, 'F', 'black', 'good', 0);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ( 'Grinch', 22, 'M', 'black', 'good', 1);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ( 'Wendy', 4, 'F', 'black', 'good', 1);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ( NULL, 9, 'M', 'black', 'good', 0);
